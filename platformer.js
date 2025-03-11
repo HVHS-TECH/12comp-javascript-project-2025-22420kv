6ab3b0cfb85f9406bcfb252011bea7871ba0cba5
22420kv <22420kv@hvhs.school.nz>
2025-03-11 00:32:10 +0000

    UPDATE 2

--- platformer.js
+++ platformer.js
@@ -6,13 +6,60 @@
 /*******************************************************/
 // Variables
 /*******************************************************/
-//const GAMEWIDTH = 900;
-//const GAMEHEIGHT = 500;
+
+const PLAYERSPEED = 5;
+const PLAYERJUMPHEIGHT = 8;
 
 /*******************************************************/
 // setup()
 /*******************************************************/
-cnv = new Canvas(900, 500);
+function setup() {
+    cnv = new Canvas(windowWidth, windowHeight);
+    world.gravity.y = 10;
+    player = new Sprite(windowWidth/2, windowHeight/2, 100, 'd');
+    player.color = 'yellow';
+    player.friction = 0;
+    
+    walls();
+}
+
+/*******************************************************/
+// walls()
+//called by setup()
+//Place holders so sprite doesnt disapear into the void
+/*******************************************************/
+function walls() {
+    wallLH = new Sprite(1, windowHeight/2, 8, height, 'k');
+	   wallLH.color = 'blue';
+	wallRH = new Sprite(1920, windowHeight/2, 8, height, 'k');
+	   wallRH.color = 'blue';
+	wallTop = new Sprite(windowWidth/2, 0, width, 8, 'k');
+	   wallTop.color = 'blue';
+	wallBottom = new Sprite(windowWidth/2, 910, width, 8, 'k' );
+	   wallBottom.color = 'blue';
+}
 
 
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+    background("Lightblue");
 
+    ///Keyboard movement keys///
+    if (kb.pressing('a')) {
+		player.vel.x = -PLAYERSPEED;
+    } else if (kb.pressing('d')) {
+        player.vel.x = PLAYERSPEED;
+    } else {
+        player.vel.x = 0;
+    }
+    if (kb.presses('w')) {
+        player.vel.y = -PLAYERJUMPHEIGHT;
+    } 
+}
+
+/*******************************************************/
+// Copied from Stack Overflow website to hide scroll bars 
+/*******************************************************/
+document.body.style.overflow = 'hidden';
