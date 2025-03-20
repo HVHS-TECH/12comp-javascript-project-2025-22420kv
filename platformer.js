7de7fa09ab4d1fc8f92efe25207e460345be84e8
22420kv <22420kv@hvhs.school.nz>
2025-03-20 22:00:51 +0000

    UPDATE 8

--- platformer.js
+++ platformer.js
@@ -13,12 +13,12 @@ let grass, water, dirt;
 const PLAYERSPEED = 1;
 const PLAYERJUMPHEIGHT = 1.5;
 
-//var score = 0;
+var score = 0;
 
 function preload() {
-    //imgPlatform  = loadImage('../images/betterPlatforms.png');
     sheetImg = loadImage("../assets/cavesofgallet_tiles.png");
-  }
+    coinImg = loadImage("../assets/smallCoin.png");
+}
 
 /*******************************************************/
 // setup()
@@ -38,6 +38,11 @@ function setup() {
     grass.addAni({ w:8, h:8, row:1, col:1 });
     grass.tile = 'g';
 
+    coin = new Group();
+    coin.image = (coinImg);
+    coin.collider = "static";
+    coin.tile = 'c';
+
     new Tiles(
         [
             'ggggggg',
@@ -46,6 +51,7 @@ function setup() {
             'ggg',
             '  gggggg',
             '        gggggg',
+            '                  c',
             '               gggg',
             '          gggggg',
             '     ggggg',
@@ -53,6 +59,7 @@ function setup() {
             '   ggg',
             '      g',
             '         g',
+            '             c',
             '           g',
             '               g',
             '                  g'
@@ -62,15 +69,19 @@ function setup() {
     )
 
     playerDeath();
-    //walls();
-    //platforms();
-    //coins();
 
-    //player.collides(coin, getPoint);
-	//function getPoint(collider1, collider2) {
-	//	collider2.remove();
-	//	score++;
-	///}
+    //point = new Sprite(100, 10, 7, 'k')           //func removeTile(position):
+                                                    //    $Tilemap.set_cell(position.x,positon.y,-1)
+
+    //funcrion removeCoin(position) {
+    //    coin.remove(position.x, position.y, -1)
+    //}
+
+    player.collides(coin, getPoint);        
+	function getPoint(collider1, collider2) {
+		coin.remove();
+		score++;
+	}
 }
 
 function playerDeath() {
@@ -79,44 +90,13 @@ function playerDeath() {
     
 }
 
-/******************************************************/
-// walls()
-//called by setup()
-//Place holders so sprite doesnt disapear into the void
-/*******************************************************/
-//function walls() {
-//    wallLH = new Sprite(1, windowHeight/2, 8, height, 'k');
-//	   wallLH.color = 'blue';
-//	wallRH = new Sprite(1920, windowHeight/2, 8, height, 'k');
-//	   wallRH.color = 'blue';
-//	wallTop = new Sprite(windowWidth/2, 0, width, 8, 'k');
-//	   wallTop.color = 'blue';
-//	wallBottom = new Sprite(windowWidth/2, 910, width, 8, 'k' );
-//	   wallBottom.color = 'blue';
-//}
-
-//function coins() {
-//    coin = new Sprite(500, 500, 20, 'k');
-//}
-
-/*******************************************************/
-// platforms()
-//called by setup()
-/*******************************************************/
-//function platforms() {
-//    platform1 = new Sprite(300, 750, 500, 50, 'k');
-//    platform2 = new Sprite(100, 600, 250, 50, 'k');
-//    platform3 = new Sprite(680, 400, 250, 50, 'k');
-//    platform4 = new Sprite(1200, 400, 100, 50, 'k');   //Continue making sprites collide with images//
-//}
-
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
     background("#21263f");      //#21263f background color to match tiles
     kbMovement();
-    //displayScore();
+    displayScore();
     if (player.collide(killBlock)) {
         player.pos.y = '10';
         player.pos.x = '60';
@@ -139,6 +119,7 @@ function kbMovement() {
     if (kb.presses('w')) {  
         player.vel.y = -PLAYERJUMPHEIGHT;
     }
+
 }
 
 
@@ -151,10 +132,10 @@ function kbMovement() {
 //}
 
 
-//function displayScore() {
-//    text("Score: "+ score, 10, 25) 
-//    textSize(20);
-//}
+function displayScore() {
+    text("Score: "+ score, 350, 10) 
+    textSize(10);
+}
 
 /*******************************************************/
 // Copied from Stack Overflow website to hide scroll bars 
